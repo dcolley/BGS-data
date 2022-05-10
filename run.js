ea18a64426144286e117755e272007fe71f37d9d
Derek Colley <derek@colley.cc>
2022-05-10 18:17:41 +0100

    Add files via upload

---
+++ run.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const fs = require('fs');
+
+(async () => {
+
+    var data = fs.readFileSync('lookups.csv', 'utf-8')
+    // console.log(lines)
+    const lines = data.split(/\r?\n/);
+    lines.forEach(async function(line) {
+        // console.log(line)
+        var parts = line.split(',')
+        var easting = parts[0]
+        var northing = parts[1]
+        // let easting = "0"
+        // let northing = "0"
+        var baseUrl = "http://webapps.bgs.ac.uk/data/webservices/CoordConvert_LL_BNG.cfc" 
+            + `?method=BNGtoLatLng&easting=${easting}&northing=${northing}`;
+        let result = await axios.get(baseUrl)
+        // console.debug(result.data)
+        console.log(`"${easting}","${northing}","${result.data.LONGITUDE}","${result.data.LATITUDE}"`)
+            
+    });
+
+})();
